2311b64b475d7e0aa72fb54a3645e267a2e5a927
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2024-03-07 16:25:30 +0900

    chore: change background mode in the ios example app
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Bucketeer
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Add the code to enable background tasks
+    if #available(iOS 13.0, tvOS 13.0, *) {
+        BKTBackgroundTask.enable()
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
